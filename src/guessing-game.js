743184a304f4e27f50bc0239f4b03122d0fce9bd
Alina Sergeenkova <autoflame.by@gmail.com>
2017-01-28 01:26:15 +0200

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.item = null;
+    this.right = null;
+    this.left = null;}
 
     setRange(min, max) {
+        this.item = (min+max)/2
+            this.left = min;
+        this.right = max;
 
     }
 
     guess() {
+return this.item;
+    }
+
+    lower() { this.left this.item
 
     }
 
-    lower() {
-
-    }
-
-    greater() {
+    greater() { this.item this.right
 
     }
 }
